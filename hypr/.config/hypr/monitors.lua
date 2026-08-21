3de3eb9acbb8c3b18cd0cc873f4bb9a605e5b887
moonbrooke <smoeland@gmail.com>
2026-08-21 17:47:34 +0700

    hypr: monitor config

--- hypr/.config/hypr/monitors.lua
+++ hypr/.config/hypr/monitors.lua
@@ -5,10 +5,23 @@ local omarchy_gdk_scale = 1
 local omarchy_monitor_scale = 1
 
 hl.env("GDK_SCALE", tostring(omarchy_gdk_scale))
-hl.monitor({ output = "", mode = "preferred", position = "auto", scale = omarchy_monitor_scale })
+-- hl.monitor({ output = "", mode = "preferred", position = "auto", scale = omarchy_monitor_scale })
 
 -- Configure a specific monitor.
 -- hl.monitor({ output = "DP-2", mode = "2560x1440@144", position = "0x0", scale = 1 })
 
 -- Portrait/rotated secondary monitor (transform: 1 = 90°, 3 = 270°).
 -- hl.monitor({ output = "DP-2", mode = "preferred", position = "auto", scale = 1, transform = 1 })
+
+hl.monitor({
+    output = "eDP-1",
+    mode = "highrr",
+    position = "0x0",
+    scale = 1,
+})
+hl.monitor({
+    output = "HDMI-A-1",
+    mode = "2560x1440@75",
+    position = "1920x-360",
+    scale = 1.25,
+})
